ef28f2e875a7e183f75438737b821823aacb2029
DamianoCasolari <damianocasolari@gmail.com>
2023-04-13 23:52:50 +0200

    create tree structure

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,38 @@
 
 </script>
 
-<template></template>
+<template>
+    <header id="app_header">
+        <div class="container_header container-lg d-flex justify-content-between align-items-center py-4 px-3">
+            <div class="container_img">
+                <img src="./assets/img/dc-logo.png" alt="logo-dc">
+            </div>
 
-<style scoped></style>
+            <nav>
+                <div class="d-md-none"><i class="fa-solid fa-mask"></i>we</div>
+                <div class="nav_option fw-bold d-none d-md-flex">
+                    <div>
+                        <a class=" text-decoration-none text-dark" href="#">Lorem, ipsum.</a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+    </header>
+    <main id="app_main">
+
+        <div class="extra_options">
+
+        </div>
+
+    </main>
+    <footer id="appfooter">
+
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+.container_header {
+    height: 115px;
+}
+</style>
