fe8f5c23de40717e10ddedf2101dade342518cde
DamianoCasolari <damianocasolari@gmail.com>
2023-04-14 18:17:44 +0200

    add layout top footer

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -14,12 +14,67 @@ export default {
 
             <div class="top_footer my_background">
                 <div class="info_top_footer">
-                    <div class="link_list_footer"></div>
-                    <div class="img_contaiener">
-                        <img src="" alt="">
+                    <div class="img_container d-flex pt-3 pt-lg-0 container-lg">
+                        <div class="col-2">
+                            <div class="link_list_footer mx-3">
+                                <h2 class=" my-lg-3">DC COMICS</h2>
+                                <ul>
+                                    <li>ciao</li>
+                                    <li>ciao</li>
+                                    <li>ciao</li>
+                                    <li>ciao</li>
+                                    <li>ciao</li>
+                                    <li>ciao</li>
+                                    <li>ciao</li>
+                                </ul>
+                            </div>
+                            <div class="link_list_footer mx-3">
+                                <h2 class="my-lg-3">shop</h2>
+                                <ul>
+                                    <li>ciao</li>
+                                    <li>ciao</li>
+
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-2 ">
+                            <div class="link_list_footer mx-3">
+                                <h2 class="my-lg-3">DC</h2>
+                                <ul>
+                                    <li>ciao</li>
+                                    <li>ciao</li>
+                                    <li>ciao</li>
+                                    <li>ciao</li>
+                                    <li>ciao</li>
+                                    <li>ciao</li>
+                                    <li>ciao</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-2">
+                            <div class="link_list_footer mx-3">
+                                <h2 class="my-lg-3">sites</h2>
+                                <ul>
+                                    <li>ciao</li>
+                                    <li>ciao</li>
+                                    <li>ciao</li>
+                                    <li>ciao</li>
+                                    <li>ciao</li>
+                                    <li>ciao</li>
+                                    <li>ciao</li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
+
+
+
+
+
+
+
             <div class="bottom_footer container-lg d-flex justify-content-between align-items-center py-4 px-3">
                 <div class="link_container">
                     <a href="#">SIGN-UP NOW!</a>
